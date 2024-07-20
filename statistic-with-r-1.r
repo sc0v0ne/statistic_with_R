204fe865a44b6bcd762d393757daae346856ca7a
sc0v0ne <56802572+sc0v0ne@users.noreply.github.com>
2024-07-20 09:19:00 -0300

    Kaggle Notebook | statistic_with_R_1 | Version 1

---
+++ statistic-with-r-1.r
@@ -0,0 +1,79 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "9b541c29",
+   "metadata": {
+    "papermill": {
+     "duration": 0.002238,
+     "end_time": "2024-07-20T12:18:49.562508",
+     "exception": false,
+     "start_time": "2024-07-20T12:18:49.560270",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "# Statistic with R - Part 1\n",
+    "\n",
+    "-----"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "a8929337",
+   "metadata": {
+    "_execution_state": "idle",
+    "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
+    "papermill": {
+     "duration": 0.001557,
+     "end_time": "2024-07-20T12:18:49.565852",
+     "exception": false,
+     "start_time": "2024-07-20T12:18:49.564295",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kaggle": {
+   "accelerator": "none",
+   "dataSources": [],
+   "isGpuEnabled": false,
+   "isInternetEnabled": true,
+   "language": "r",
+   "sourceType": "notebook"
+  },
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.4.0"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.713877,
+   "end_time": "2024-07-20T12:18:49.690183",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2024-07-20T12:18:45.976306",
+   "version": "2.6.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
