484b4f9c522175b31a7104b65256cfdc4ff3d211
sc0v0ne <56802572+sc0v0ne@users.noreply.github.com>
2024-07-20 11:26:51 -0300

    Kaggle Notebook | statistic_with_R_1 | Version 2

--- statistic-with-r-1.r
+++ statistic-with-r-1.r
@@ -2,13 +2,13 @@
  "cells": [
   {
    "cell_type": "markdown",
-   "id": "9b541c29",
+   "id": "dd881009",
    "metadata": {
     "papermill": {
-     "duration": 0.002238,
-     "end_time": "2024-07-20T12:18:49.562508",
+     "duration": 0.00194,
+     "end_time": "2024-07-20T14:26:43.731497",
      "exception": false,
-     "start_time": "2024-07-20T12:18:49.560270",
+     "start_time": "2024-07-20T14:26:43.729557",
      "status": "completed"
     },
     "tags": []
@@ -16,21 +16,39 @@
    "source": [
     "# Statistic with R - Part 1\n",
     "\n",
-    "-----"
+    "-----\n",
+    "\n",
+    "## O que estatistica ?\n",
+    "\n",
+    "A estatistica trata-se de uma ciência, onde utilizamos informações do nosso mundo para dar respostas a perguntas criadas. Mas como assim ciência ? Utilizar a estatistica não basta apenas coletar os dados(informações), os dados precisam ser estudados, compreendidos e processados para conseguir um resultado final. Todo esse processo até chegar ao resultado, traz um aprendizado de grande valor. Onde em cada ponto que você manipula pode trazer um resultado diferente. Não ache isso ruim, chega ser um desafio, tendo que testar e avaliar novamente até chegar em um resultado eficaz. \n",
+    "\n",
+    "Dentro da estatistica possui diversas ferramentas visuais, matemáticas, coleta e softwares. Para conseguirmos atender a diversos problemas do mundo real. O mais incrível a estatistica e interdisciplinar, todas as áreas possui que você pensar vai ter algo conectado estatistica, pegando do mais simples  o jornal da tv apresenta a previsão do tempo, o tempo está em constante mudança mas por meio da coleta de dados existe já padrões conhecidos utilizados para realizar uma previsão do tempo no dia seguinte, indo mais além, entender o espaço de exemplo as super novas, utilizando as já conhecidas para realizar a medição de distâncias cósmicas.\n",
+    "\n",
+    "## Porque devemos estudar estatistica ?\n",
+    "\n",
+    "Aprendendo estatistica podemos compreender melhor problemas do nosso mundo, onde apenas observação não dara respotas, utilizando a coleta e o estudo sobre os dados. Podemos encontrar mais dados onde não conseguimos visualizar sem um processamento adequado. \n",
+    "\n",
+    "## Porque utilizar linguagem R ?\n",
+    "\n",
+    "A linguagem R, trata-se de uma linguagem de programação focada em estatistica. Possuindo um grande variedade de algoritmos e funções para aplicar em diversos problemas estatisticos. Sendo possível explorar conjuntos de dados, tratar os dados, visualizar e mais diversos recursos disponíveis em sua documentação. Além do mais o projeto e código aberto sendo possível qualquer ser humano a consultar o código e auxiliar no desenvolvimento e evolução do projeto.\n",
+    "\n",
+    "- [The R Project for Statistical Computing](https://www.r-project.org)\n",
+    "\n",
+    "\n"
    ]
   },
   {
    "cell_type": "code",
    "execution_count": null,
-   "id": "a8929337",
+   "id": "aaafe902",
    "metadata": {
     "_execution_state": "idle",
     "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
     "papermill": {
-     "duration": 0.001557,
-     "end_time": "2024-07-20T12:18:49.565852",
+     "duration": 0.001377,
+     "end_time": "2024-07-20T14:26:43.734570",
      "exception": false,
-     "start_time": "2024-07-20T12:18:49.564295",
+     "start_time": "2024-07-20T14:26:43.733193",
      "status": "completed"
     },
     "tags": []
@@ -63,14 +81,14 @@
   },
   "papermill": {
    "default_parameters": {},
-   "duration": 3.713877,
-   "end_time": "2024-07-20T12:18:49.690183",
+   "duration": 3.267764,
+   "end_time": "2024-07-20T14:26:43.860522",
    "environment_variables": {},
    "exception": null,
    "input_path": "__notebook__.ipynb",
    "output_path": "__notebook__.ipynb",
    "parameters": {},
-   "start_time": "2024-07-20T12:18:45.976306",
+   "start_time": "2024-07-20T14:26:40.592758",
    "version": "2.6.0"
   }
  },
