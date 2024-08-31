1c325d53aeef74266b6211de96e9ae33b06d3445
sc0v0ne <sc0v1n001@gmail.com>
2024-08-31 14:03:22 -0300

    Testing Shiny

---
+++ statistic-with-r-3.r
@@ -0,0 +1,9 @@
+# INSTALL -> install.packages("shiny")
+# INSTALL -> install.packages("bslib")
+
+
+# reference -> https://shiny.posit.co/r/getstarted/shiny-basics/lesson1/index.html
+
+
+library(shiny)
+runExample("01_hello")
